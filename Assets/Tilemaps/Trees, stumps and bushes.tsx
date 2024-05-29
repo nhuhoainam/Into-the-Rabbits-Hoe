233d7f028f7bea59cb8d29f9a9ea9483c9393a50
Hoai Nam <hoainamnguyennhu7@gmail.com>
2024-05-29 21:45:33 +0700

    add tiled to project

---
+++ Assets/Tilemaps/Trees, stumps and bushes.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="Trees, stumps and bushes" tilewidth="16" tileheight="16" tilecount="84" columns="12">
+ <image source="../Sprout Lands/Objects/Trees, stumps and bushes.png" width="192" height="112"/>
+</tileset>
