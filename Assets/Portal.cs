f97cb42d1420e737471d24f51e5fd508026c4884
zrefalogia <vuongquocphongv@gmail.com>
2024-06-12 14:27:36 +0700

    add scene transitions + scene titles

--- Assets/Portal.cs
+++ Assets/Portal.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,13 +8,22 @@ public class Portal : MonoBehaviour
     [SerializeField] private string spawnPointName; // Name of the spawn point to use
     private bool isTriggered = false; // Flag to prevent multiple triggers
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private IEnumerator OnTriggerEnter2D(Collider2D other)
     {
         if (!isTriggered && other.gameObject.GetComponent<PlayerController>())
         {
             isTriggered = true; // Set the flag to true to prevent re-entry
+            var transAnim = GameObject.FindWithTag("TransitionAnimation");
+            if (transAnim == null)
+            {
+                Debug.LogWarning("No transition animation found in the scene");
+                yield break;
+            }
             PlayerPrefs.SetString("SpawnPoint", spawnPointName);
+            transAnim.GetComponent<Animator>().SetTrigger("End");
+            yield return new WaitForSeconds(1.0f);
             SceneManager.LoadScene(sceneToLoad);
+            transAnim.GetComponent<Animator>().SetTrigger("Start");
         }
     }
 }
