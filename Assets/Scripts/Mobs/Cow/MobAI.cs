34a9b6b85c6691c735e6c6ea9f742cc7f3df8149
ltp1405 <lamtungpro1405@gmail.com>
2024-07-03 17:01:37 +0700

    update cow AI

--- Assets/Scripts/Mobs/Cow/MobAI.cs
+++ Assets/Scripts/Mobs/Cow/MobAI.cs
@@ -2,138 +2,109 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using Animancer;
 using Pathfinding;
-
-class EatingState : IMobAIState
-{
-    private readonly MobAI mobAI;
-    private float time = 0;
-    public EatingState(MobAI mobAI)
-    {
-        this.mobAI = mobAI;
-    }
-    public void StateEnter()
-    {
-        time = 0;
-        mobAI.animator.SetBool("isEating", true);
-    }
-
-    public void StateUpdate()
-    {
-        time += Time.deltaTime;
-        if (time >= mobAI.eatingTime)
-        {
-            mobAI.animator.SetBool("isEating", false);
-            mobAI.animator.SetBool("isChewing", true);
-        }
-    }
-}
+using Unity.IO.LowLevel.Unsafe;
 
 
-class WanderingState : IMobAIState
-{
-    private readonly MobAI mobAI;
-    public WanderingState(MobAI mobAI)
-    {
-        this.mobAI = mobAI;
-    }
-    public void StateEnter()
-    {
-        mobAI.animator.SetBool("isWalking", true);
-    }
 
-    public void StateUpdate()
-    {
-        mobAI.animator.SetBool("isWalking", true);
-    }
-}
-
-class SleepingState : IMobAIState
-{
-    private readonly MobAI mobAI;
-    public SleepingState(MobAI mobAI)
-    {
-        this.mobAI = mobAI;
-    }
-    public void StateEnter()
-    {
-        mobAI.animator.SetBool("isSleeping", true);
-    }
-
-    public void StateUpdate()
-    {
-        mobAI.animator.SetBool("isSleeping", true);
-    }
-}
-
-class StandingState : IMobAIState
-{
-    private readonly MobAI mobAI;
-    public StandingState(MobAI mobAI)
-    {
-        this.mobAI = mobAI;
-    }
-    public void StateEnter()
-    {
-        mobAI.animator.SetBool("isStanding", true);
-    }
-
-    public void StateUpdate()
-    {
-        mobAI.animator.SetBool("isStanding", true);
-    }
-}
-
-class FindingCropState : IMobAIState
-{
-    private readonly MobAI mobAI;
-    public FindingCropState(MobAI mobAI)
-    {
-        this.mobAI = mobAI;
-    }
-    public void StateEnter()
-    {
-        mobAI.animator.SetBool("isFindingCrop", true);
-    }
-
-    public void StateUpdate()
-    {
-        mobAI.animator.SetBool("isFindingCrop", true);
-    }
-}
 public class MobAI : MonoBehaviour
 {
     [SerializeField] internal float eatingTime = 5;
-    internal Animator animator;
+    [SerializeField] internal AnimancerComponent animancer;
+    [SerializeField] internal AnimationClip standingAndWavingTail;
+    [SerializeField] internal AnimationClip standingAndBlinking;
+    [SerializeField] internal AnimationClip eating;
+    [SerializeField] internal AnimationClip walking;
+    [SerializeField] internal AnimationClip sleeping;
+    [SerializeField] internal AnimationClip chewing;
+
     internal Seeker seeker;
     internal AIPath aiPath;
 
-    internal IMobAIState currentState;
-    private enum State
-    {
-        Done,
-        FindingCrop,
-        StandingAndWavingTail,
-        StandingAndBlinking,
+    [SerializeField] private int hunger = 10000;
+    [SerializeField] private int sleepiness = 10000;
+
+    enum State {
+        Idling,
+        Sleeping,
         Eating,
         Wandering,
-        Sleeping,
-        Chewing,
+        Chewing
     }
 
     private State state;
-    // Start is called before the first frame update
+
     void Start()
     {
         aiPath = GetComponent<AIPath>();
         seeker = GetComponent<Seeker>();
-        animator = GetComponent<Animator>();
-        currentState = new StandingState(this);
-
-        // StartCoroutine(Wander());
+        state = State.Idling;
+        StartCoroutine(IdlingAction());
     }
-    void Update()
-    {
-        currentState.StateUpdate();
+
+    private IEnumerator FindFood() {
+        state = State.Eating;
+
+        animancer.Play(walking);
+        aiPath.destination = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+        yield return new WaitForSeconds(Random.Range(5, 10));
+
+        animancer.Play(eating);
+        yield return new WaitForSeconds(eating.length);
+        animancer.Play(chewing);
+        yield return new WaitForSeconds(chewing.length);
+
+        state = State.Idling;
+    }
+
+    private IEnumerator IdlingAction() {
+        while (true) {
+            int action = Random.Range(0, 3);
+            if (state == State.Idling) {
+                if (action == 0) {
+                    animancer.Play(standingAndBlinking).Time = 0;
+                } else if (action == 1) {
+                    animancer.Play(standingAndWavingTail).Time = 0;
+                } else if (action == 2) {
+                    StartCoroutine(Wandering());
+                }
+            }
+            yield return new WaitForSeconds(Random.Range(2, 6));
+        }
+    }
+
+    private IEnumerator Wandering() {
+        state = State.Wandering;
+        animancer.Play(walking);
+        yield return new WaitForSeconds(Random.Range(2, 5));
+        state = State.Idling;
+    }
+
+    private IEnumerator Sleeping() {
+        state = State.Sleeping;
+        animancer.Play(sleeping);
+        yield return new WaitForSeconds(Random.Range(10, 20));
+        sleepiness = 10000;
+        state = State.Idling;
+    }
+
+    void Update() {
+        if (hunger > 0 && state == State.Wandering) {
+            hunger -= Random.Range(3, 7);
+        } else if (hunger > 0) {
+            hunger -= Random.Range(1, 3);
+        }
+        if (sleepiness > 0) {
+            sleepiness -= Random.Range(1, 3);
+        }
+
+        if (hunger <= 0 && state == State.Idling) {
+            StartCoroutine(FindFood());
+        }
+
+        if (sleepiness <= 0 && state == State.Idling) {
+            StartCoroutine(Sleeping());
+        }
     }
 }
--- Assets/Scripts/Mobs/Cow/MobAIState.cs
+++ Assets/Scripts/Mobs/Cow/MobAIState.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IMobAIState
+public abstract class MobAIState
 {
+    public MobAIState(MobAI mobAI)
+    {
+        this.MobAI = mobAI;
+    }
+
+    protected MobAI MobAI { get; }
     // Start is called when the state is entered
-    void StateEnter();
+    abstract public void StateEnter();
 
     // Update is called when the state is active, system will call this method every frame
-    void StateUpdate();
+    abstract public void StateUpdate();
+
+    // Exit is called when the state is exited
+    abstract public void StateExit();
 }
