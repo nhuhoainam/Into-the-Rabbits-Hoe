03595a1f9fde19625a15c6eacc2b413c2676b0f0
ltp1405 <LamTungPro1405@gmail.com>
2024-07-10 15:12:21 +0700

    separate prefab for each type of rocks

---
+++ Assets/Scripts/Rock/Rock.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour, IPlayerInteractable
+{
+    [SerializeField] private int health = 3;
+    // Start is called before the first frame update
+    void IPlayerInteractable.Interact(PlayerData playerData)
+    {
+        if (health > 0)
+        {
+            health--;
+            if (health == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
