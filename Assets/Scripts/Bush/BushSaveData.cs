275a5ff464f4fe53fdf29333aecb1fc461d29f9a
ltp1405 <LamTungPro1405@gmail.com>
2024-07-24 15:14:58 +0700

    Update BushSaveData.cs

--- Assets/Scripts/Bush/BushSaveData.cs
+++ Assets/Scripts/Bush/BushSaveData.cs
@@ -8,8 +8,10 @@ public class BushSaveData
     public bool hasBerries;
     public float lastCheckBerrySpawnTime;
     public string key;
-    public BushSaveData(bool hasBerries, float lastCheckBerrySpawnTime, string key)
+    public Vector3 position;
+    public BushSaveData(Vector3 position, bool hasBerries, float lastCheckBerrySpawnTime, string key)
     {
+        this.position = position;
         this.hasBerries = hasBerries;
         this.lastCheckBerrySpawnTime = lastCheckBerrySpawnTime;
         this.key = key;
